24bc1985dac4aa2d663dca8d900f1935e72fbda6
marinajacks <chenbiaozainan@163.com>
2019-10-25 18:38:53 +0800

    脚本修改

--- offer.py
+++ offer.py
@@ -1,15 +1,10 @@
 #面试题21:调整数组顺序使奇数位于偶数前面
-
-
-
-import random 
-a=[]
-for i in range(10):
-    a.append(random.randint(1,100))
-b=[]
-for i in range(len(a)):
-    if(i%2==0):
-        b.append(a[i])
-for i in range(len(a)):
-    if(i%2==1):
-        b.append(a[i])
+def ReOrder(a):
+    b=[]
+    for i in range(len(a)):
+        if(i%2==0):
+            b.append(a[i])
+    for i in range(len(a)):
+        if(i%2==1):
+            b.append(a[i])
+    return b
