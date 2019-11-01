6f2c167c848c0e78002719905415058f5aa8d2b1
marinajacks <chenbiaozainan@163.com>
2019-11-01 22:13:52 +0800

    修改一些常见的算法

--- data_structure.py
+++ data_structure.py
@@ -16,7 +16,11 @@ def arrays(n):
         a.append(random.randint(0,100))
     a.sort()
     return a
-
+def arrayss(n):
+    a=[]
+    for i in range(n):
+        a.append(random.randint(0,100))
+    return a
 def addarrays(a,b):
     c=[]
     i=0
@@ -35,7 +39,7 @@ def addarrays(a,b):
         c=c+b[j:len(b)]
     return c
         
-if __name__=="__main__":
+#if __name__=="__main__":
     #使用空格格式化输入两个正整数，分别作为两个list的长度
     #a,b的长度都不能是空值
     a=list(map(int,input().split(' ')))
@@ -48,4 +52,41 @@ if __name__=="__main__":
     print(c)
     
 
+
+
+'''在下面我们会对几个排序算法进行复习，尤其是几个关键的排序算法，例如：冒泡排序，选择排序，
+插入排序，归并排序、快速排序和堆排序等相关的排序算法。我们需要对这些算法的时间复杂度和空间
+复杂度以及对应的算法的实现进行一番的分析和相关的介绍。
+'''
+#快速排序
+'''
+快速排序是一个经典的使用分治+递归的方式来求解的方式，这个方式的主要处理方法是。
+首先，找到一个标识，假设这个标识是第一个元素，我们以这个数作为标准，对整个数组进行排序。
+设置两个指针，分别是从前出发left和从后出发的标签right，标识数据位置的指针。
+1.从数组最后一位出发，找到比这个标签小的第一个数据，将右边的指针right指向该位置；
+2.从数组的第一位出发，找到比这个标签大的第一个数据，将左边的指针left指向该位置；
+3.如果lfet和right仅仅相差1，那么就将最初的那个标识放到中间。
+分别对左边部分和右边部分重复上面的快速排序算法。指导子数组的长度仅仅为1为止。
+'''
+
+def quicksort(data):
+    if(len(data)>=2):
+        flag=data[0]
+        left,right=[],[]
+        data.remove(flag)
+        for num in data:
+            if num>=flag:
+                right.append(num)
+            else:
+                left.append(num)
+        return quicksort(left)+[flag]+quicksort(right)
+    else:
+        return data
+        
+if __name__=="__main__":
+    a=arrayss(10)
+    print(a)
+    print(quicksort(a))
+    
+        
     
