43b90c641fae9e789a46c543834b19b1650c3a70
marinajacks <chenbiaozainan@163.com>
2020-03-07 16:33:30 +0800

    提升脚本

--- java/Test/src/com/ecnu/Maths.java
+++ java/Test/src/com/ecnu/Maths.java
@@ -2,10 +2,11 @@ package com.ecnu;
 
 public class Maths {
 	public static void main(String [] args) {
-		System.out.println("hello world!");
-		double[] a= {21000,20000,181292,129207,155853,-120,-100};
-		System.out.println(sums(a));
+		int [] a= {1};
+		System.out.println(searchInsert(a,1));
+		
 	}
+
 	public static double sums(double[] nums) {
 		double adds=0;
 		for(int i=0;i<nums.length;i++) {
@@ -13,4 +14,32 @@ public class Maths {
 		}
 		return adds;
 	}
-}
+
+	public static int searchInsert(int[] nums, int target) {
+        int left=0,right=nums.length-1,index=(left+right)/2;
+        System.out.println(left+"\t"+right+"\t"+index);
+        if(target>nums[right]) {
+        	index= right+1;
+        }
+        else if(target<nums[left]) {
+        	index=0;
+        }
+        else {
+        	while(left<=right){
+        		if(nums[index]>target){
+        			right=index-1;
+        			index=(left+right)/2;
+        			
+        		}
+        		else if(nums[index]<target){
+        			left=index+1;
+        			index=(left+right)/2;
+        		}
+        		else {
+        			return index;
+        		}
+        	}	
+		}        	
+        return index+1;
+	}
+	}
